001968f728f53f5581d4e76791cc24e96400d263
Emilio Platzer <emilioplatzer@gmail.com>
2016-09-26 20:48:43 -0300

    Anda hasta h1 inclusive

--- epjson.js
+++ epjson.js
@@ -2,6 +2,14 @@
 
 var epjson = {};
 
+var SymbolInternal='394fjasifqwenvnv';
+
+function SymbolInternalx(){
+    throw new Error('this is a symbol, not a function');
+}
+
+var tooMuch=10;
+
 epjson.replacer = function replacer(key, value){
     // if(key==='$special'){
     //     return {$escape: }
@@ -11,6 +19,9 @@ epjson.replacer = function replacer(key, value){
     // }
     var realValue=this==null?null:this[key];
     console.log(['xxx replacer'],key,value, typeof value, this[key], '->', realValue, typeof realValue);
+    // if(realValue===SymbolInternal){
+    //     return undefined;
+    // }
     if(realValue===undefined || realValue!==null && realValue instanceof Function){
         console.log('xxx UNDEFINED replacer',key,value, typeof value);
         return {$special: "undefined"};
@@ -19,25 +30,50 @@ epjson.replacer = function replacer(key, value){
         console.log('typeof realValue', typeof realValue, realValue)
         return value;
     }
-    if('$special' in realValue){
-        console.log("$$$$SPECIAL")
-        return JSON.stringify({$escape: value});
+    if(key==='$escape'){
+        return value;
+    }
+    if('$special' in realValue /*&& realValue.$internal!==SymbolInternal*/){
+        console.log("$$$$SPECIAL", realValue)
+        // return JSON.stringify({$escape: value});
+        if(!tooMuch--){
+            throw new Error('too much');
+        }
+        return {$escape: realValue/*, $internal:SymbolInternal*/};
     }
     if(realValue!=null && realValue instanceof Date){
         console.log('xxx THIS KEY replacer',key,value, typeof value);
-        return {$special:'Date', $date: realValue.getTime()};
+        return {$special:'Date', $date: realValue.getTime()/*, $internal:SymbolInternal*/};
+    }
+    return value;
+}
+
+epjson.reviver2 = function reviver(key, value){
+    const prefix = '{"$special":';
+    console.log(['xxx reviver'],key,value, typeof value, '==>', typeof value === 'string'?value.substring(0,prefix.length):'');
+    if(typeof value === 'string' && value.substring(0,prefix.length)===prefix){
+        var specialObject = epjson.parse(value);
+        console.log('ZZZZZ special',specialObject)
+        if(specialObject.$special=='Date'){
+            return new Date(value.$date);
+        }else if(specialObject.$special=='undefined'){
+            return undefined;
+        }
+        return specialObject;
     }
     return value;
 }
 
 epjson.reviver = function reviver(key, value){
     console.log('xxx reviver',key,value, typeof value);
-    if(value!=null && value.$special){
+    if(key!=='$escape' && value!=null && value.$special){
         if(value.$special=='Date'){
             return new Date(value.$date);
         }else if(value.$special=='undefined'){
             return undefined;
         }
+    }else if(/*key!=='$escape' &&*/ value!=null && value.$escape){
+        return value.$escape;
     }
     return value;
 }
--- test/test.js
+++ test/test.js
@@ -29,8 +29,8 @@ var fixtures=[
     {name:'partes'    ,value: [parte, parte],            },
     {name:'rombo'     ,value: {a:parte, b:parte},        },
     {name:'array'     ,value: [1,"2", false],            },
-    {name:'fecha'     ,value: new Date(1969, 5-1, 6)     , expectEncode: '{"$special":"Date","$date":-20725200000}'},
-    {name:'{fecha}'   ,value: {a:1, f:new Date(2016,2,2)}},
+    {name:'fecha'     ,value: new Date(1969, 5-1, 6)     , expectEncode: '{"$special":"Date","$date":-20725200000}', check:function(o){ return o instanceof Date; }},
+    {name:'{fecha}'   ,value: {a:1, f:new Date(2016,2,2)}, check:function(o){ return o.f instanceof Date; }},
     {name:'bigNumber' ,value: 12345678901234567890,      },
     {name:'bool'      ,value: true,                      },
     {name:'null'      ,value: null,                      },
@@ -43,7 +43,9 @@ var fixtures=[
         value:{list1:[{one:{two:['the list',32,'33',null,undefined,'}'],'3':33,'length':4,d:new Date()},_:'333'}],f:function(){return 3;}},
      expected:{list1:[{one:{two:['the list',32,'33',null,undefined,'}'],'3':33,'length':4,d:new Date()},_:'333'}]                        },
     },
-    {name:'h1-EPJSON' ,value: {d:{$special:'Date',$date:1456887600000},u:{$special:'undefined'}}},
+    {name:'h1-EPJSON' ,value: {d:{$special:'Date',$date:1456887600000},u:{$special:'undefined'}}, 
+                       expectEncode: JSON.stringify({d:{$escape:{$special:'Date',$date:1456887600000}},u:{$escape:{$special:'undefined'}}}),
+    },
     {name:'h2-EPJSON' ,value: {$escape:{d:{$special:'Date',$date:1456887600000},u:{$special:'undefined'}}}},
     {name:'h3-EPJSON' ,value: {$escape:{$escape:{d:{$special:'Date',$date:1456887600000},u:{$special:'undefined'},e:{$escape:true}}}}},
     {name:'Point'     ,value: new Point(1,2,3.3), 
@@ -58,7 +60,6 @@ describe("epjson",function(){
     fixtures.forEach(function(fixture){
         if(fixture.skip) return;
         var withError=false;
-        var dif=false;
         it("fixture: "+JSON.stringify(fixture),function(){
             var encoded=EPJSON.stringify(fixture.value);
             if('expectEncode' in fixture){
@@ -66,8 +67,9 @@ describe("epjson",function(){
                 expect(encoded).to.eql(fixture.expectEncode);
             }
             var decoded=EPJSON.parse(encoded);
-            dif=!deepEqual(decoded,'expected' in fixture?fixture.expected:fixture.value);
             expect(decoded).to.eql('expected' in fixture?fixture.expected:fixture.value);
+            var eql=deepEqual(decoded,'expected' in fixture?fixture.expected:fixture.value);
+            expect(eql).to.ok();
             if('check' in fixture){
                 expect(fixture.check(decoded)).to.ok();
             }
