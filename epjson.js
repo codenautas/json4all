9ae4a9da71a7aa15b95301c9628e6d6f6b9afbb9
Emilio <emilioplatzer@gmail.com>
2016-09-26 18:48:11 -0300

    Metiendo todo dentro de $special

--- epjson.js
+++ epjson.js
@@ -3,28 +3,41 @@
 var epjson = {};
 
 epjson.replacer = function replacer(key, value){
-    console.log(['xxx replacer'],key,value, typeof value, this[key]);
-    if(key && (/*'$escape' in this || */ '$date' in this || '$undefined' in this || '$type' in this)){
-        return {$escape: value};
-    }
-    if(this[key]===undefined || this[key]!==null && this[key] instanceof Function){
+    // if(key==='$special'){
+    //     return {$escape: }
+    // }
+    // if(key && (('$special' in this || '$escape' in this) && this.$escaping!='escaping')){
+    //     return JSON.stringify({$escape: value});
+    // }
+    var realValue=this==null?null:this[key];
+    console.log(['xxx replacer'],key,value, typeof value, this[key], '->', realValue, typeof realValue);
+    if(realValue===undefined || realValue!==null && realValue instanceof Function){
         console.log('xxx UNDEFINED replacer',key,value, typeof value);
-        return {"$undefined": true};
+        return {$special: "undefined"};
     }
-    if(this[key]!=null && this[key] instanceof Date){
+    if(realValue===null || typeof realValue!=='object'){
+        console.log('typeof realValue', typeof realValue, realValue)
+        return value;
+    }
+    if('$special' in realValue){
+        console.log("$$$$SPECIAL")
+        return JSON.stringify({$escape: value});
+    }
+    if(realValue!=null && realValue instanceof Date){
         console.log('xxx THIS KEY replacer',key,value, typeof value);
-        return {$date: this[key].getTime()};
+        return {$special:'Date', $date: realValue.getTime()};
     }
     return value;
 }
 
 epjson.reviver = function reviver(key, value){
     console.log('xxx reviver',key,value, typeof value);
-    if(value!=null && value.$date){
-        return new Date(value.$date);
-    }
-    if(value!=null && value.$undefined){
-        return undefined;
+    if(value!=null && value.$special){
+        if(value.$special=='Date'){
+            return new Date(value.$date);
+        }else if(value.$special=='undefined'){
+            return undefined;
+        }
     }
     return value;
 }
