9532b1a1e35197c226a459bb411c8f243591960b
Emilio <emilioplatzer@gmail.com>
2017-01-13 17:48:20 -0300

    esprima está en dist/

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config, preConfig) {
       'node_modules/express-useragent/lib/express-useragent.js',
       'node_modules/best-globals/best-globals.js',
       'node_modules/discrepances/lib/discrepances.js',
-      'node_modules/esprima/esprima.js',
+      'node_modules/esprima/dist/esprima.js',
       // 'node_modules/self-explain/node_modules/esprima/dist/esprima.js',
       'node_modules/self-explain/dist/escodegen.browser.js',
       'node_modules/self-explain/lib/self-explain.js',
